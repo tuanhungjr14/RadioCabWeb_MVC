2df9a01f03bca4e8f06a93a42e57f940af1567fc
tuan1 <tuan1@TUANHUNGJR>
2023-10-11 23:11:56 +0700

    11/10

---
+++ RadioCab/Controllers/RadioCabsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RadioCab.Controllers
+{
+    public class RadioCabsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RadioCab/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RadioCab.Models;
+
+namespace RadioCab.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<CabCompany> CabCompanies { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Advertisment> Advertisments { get; set; }
+        public DbSet<FeedbackType> feedbackTypes { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<Pricing> Pricings { get; set; }
+        public DbSet<CabOrder> CabOrders { get; set; }
+        public DbSet<DriveOrder> DriveOrders { get; set; }
+        public DbSet<AdOrder> AdOrders { get; set; }
+
+
+    }
+}
